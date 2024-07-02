7a0b630f77b4407e83b458731ac2691953410ef3
thisissolmi <oksk6685@naver.com>
2024-07-03 01:41:30 +0900

    add command

--- A01.java
+++ A01.java
@@ -1,3 +1,27 @@
+/*
+A01 - 나이계산
+태어난 년도를 입력받은 후, 이 값을 이용하여 나이를 계산하고 출력하라.
+
+단, 나이 = <올해년도> – 태어난 년도 + 1 로 계산한다.
+
+변수는 다음과 같이 사용하라.
+
+int birth_year ; // 태어난 년도 int age ; // 나이
+
+소스에서 a01() 함수 내에 코드를 완성하라.
+
+Enter a birth year > 1999
+You are 25 years old.
+Enter a birth year > 2009
+You are 15 years old.
+*/
+
+/*
+date : 240703 Wen
+
+concept : Scanner scanner = new Scanner(System.in); 객체 생성, 
+사용자로부터 입력받은 값을 변수에 저장 -> Scanner 객체의 메서드 사용, 사용자가 정수를 입력하도록 하려면 nextInt() 메서드사용
+ */
 import java.util.Scanner;
 
 class Main {
