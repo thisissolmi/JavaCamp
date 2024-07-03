d4095b67d8037f266c03c1e9a8f93dff94bc06b3
thisissolmi <oksk6685@naver.com>
2024-07-04 02:13:41 +0900

    add info

--- A02.java
+++ A02.java
@@ -1,4 +1,18 @@
 /*
+A02 - 온도변환
+섭씨 온도를 입력받아 이 값을 화씨온도로 변환하여 출력하라.
+
+단, 화씨 온도 = 섭씨 온도 * 1.8 + 32 로 계산한다. 변수는 다음과 같이 사용하라.
+
+double c_degree; //섭씨 온도
+
+double f_degree; //화씨 온도
+
+소스에서 a02() 함수 내에 코드를 완성하라.
+
+Enter Celsius temperature > 20.5
+Fahrenheit temperature is 68.9
+
 240704
 
 
